601ac9d8fab7fb80483c1c8120b501ebafe07739
cvjetkovicdaniel140 <cvjetkovicdaniel140@gmail.com>
2025-03-14 06:34:57 +0000

    Add 10.java

---
+++ src/10.java
@@ -0,0 +1,9 @@
+// SchooLpRoject.java
+import java.util.*;
+
+public class SchooLpRoject {
+    public static void main(String[] args) {
+        // TODO: Write your code here
+        System.out.println("Hello, world!");
+    }
+}
