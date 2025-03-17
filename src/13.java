d996b02ced173c2c628a8e1f63ff1ba9a6ed8cd5
cvjetkovicdaniel140 <cvjetkovicdaniel140@gmail.com>
2025-03-17 14:27:15 +0000

    Add 13.java

---
+++ src/13.java
@@ -0,0 +1,10 @@
+import java.util.*; 
+public class SchoolProject { 
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in); 
+		int x = sc.nextInt(); 
+		double y = sc.nextDouble(); 
+		System.out.println("x is " + x); 
+		System.out.println("y is " + y); 
+	} 
+}
