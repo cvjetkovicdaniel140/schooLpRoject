7355ce7bccda9721f0680d37545bb947f10b60ae
cvjetkovicdaniel140 <cvjetkovicdaniel140@gmail.com>
2025-03-20 17:23:13 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Example program to demonstrate Java's switch-case statement
+        String color = "red";
+        
+        if (color.equals("red")) {
+            System.out.println("The value of color is red.");
+        } else if (color.equals("green")) {
+            System.out.println("The value of color is green.");
+        } else {
+            System.out.println("Unknown color: " + color);
+        }
+    }
+}
