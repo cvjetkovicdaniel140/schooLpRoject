69ac8490cc1602f9f2411e08534293740ec0fa0e
cvjetkovicdaniel140 <cvjetkovicdaniel140@gmail.com>
2025-04-26 13:08:52 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,11 @@
+public class RandomJavaExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int age = 20;
+        if (age > 18) {
+            System.out.println("You are old enough to vote.");
+        } else {
+            System.out.println("You are not old enough to vote.");
+        }
+    }
+}
